d323203de4fd175f6d284313154564db0437fc25
Markarthian <jisk.vdmeer@gmail.com>
2013-03-27 21:14:23 +0100

    Initial commit of the proxy files
    ~Boring~

---
+++ CommonProxy.java
@@ -0,0 +1,7 @@
+package markarthian.mods;
+
+public class CommonProxy {
+	public void registerRenderers(){
+		
+	}
+}
---
+++ client/ClientProxy.java
@@ -0,0 +1,10 @@
+package markarthian.mods.client;
+
+import markarthian.mods.CommonProxy;
+
+public class ClientProxy extends CommonProxy
+{
+  public void registerRenderers()
+  {
+  }
+}
